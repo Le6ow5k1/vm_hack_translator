0d81769898483817503e661a5a9165fee91eb51f
Konstantin Lazarev <le6oww5k1@gmail.com>
2020-12-27 20:51:34 +0500

    Add runner tests for memory access commands

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -17,4 +17,49 @@ describe Runner do
       expect(output).to eq(expected_output)
     end
   end
+
+  context 'BasicTest' do
+    let(:file_path) do
+      File.join(File.dirname(__FILE__), './fixtures/BasicTest.vm')
+    end
+
+    it do
+      described_class.new.call(file_path)
+
+      output = File.read(file_path + '.asm')
+      expected_output = File.read(file_path.sub('.vm', '.expected') + '.asm')
+
+      expect(output).to eq(expected_output)
+    end
+  end
+
+  context 'PointerTest' do
+    let(:file_path) do
+      File.join(File.dirname(__FILE__), './fixtures/PointerTest.vm')
+    end
+
+    it do
+      described_class.new.call(file_path)
+
+      output = File.read(file_path + '.asm')
+      expected_output = File.read(file_path.sub('.vm', '.expected') + '.asm')
+
+      expect(output).to eq(expected_output)
+    end
+  end
+
+  context 'StaticTest' do
+    let(:file_path) do
+      File.join(File.dirname(__FILE__), './fixtures/StaticTest.vm')
+    end
+
+    it do
+      described_class.new.call(file_path)
+
+      output = File.read(file_path + '.asm')
+      expected_output = File.read(file_path.sub('.vm', '.expected') + '.asm')
+
+      expect(output).to eq(expected_output)
+    end
+  end
 end
